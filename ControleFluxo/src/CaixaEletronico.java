c0ee8e167d998a606ca27ad0a96b6fcd7f0cbead
Marcos Roberto <marcosrodiasmr@gmail.com>
2023-05-23 22:58:10 -0300

    CaixaEletroniico

---
+++ ControleFluxo/src/CaixaEletronico.java
@@ -0,0 +1,12 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 15 ;
+        double valorSolicitado = 26 ;
+
+        if(saldo < valorSolicitado) 
+         saldo = saldo + valorSolicitado ;
+
+        System.out.println(saldo);
+     
+    }
+}
