07cfb2d837f1fa8451141097d70423f0a8398237
Marcos Roberto <marcosrodiasmr@gmail.com>
2023-05-23 23:23:37 -0300

    Incluido Classe ResultadoEscolar

---
+++ ControleFluxo/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6 ;
+
+        if(nota >= 7)
+         System.out.println("Aprovado");
+        else
+         System.out.println("Reprovado");
+    }
+   
+}
